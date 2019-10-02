7edbad8220b4f8a542b55a4531d4bd56b7b536b8
Håvard <havardei@nmbu.no>
2019-10-02 14:20:32 +0200

    median beginning

--- src/havard_eig_ex/ex03_project/test_median.py
+++ src/havard_eig_ex/ex03_project/test_median.py
@@ -23,6 +23,42 @@ def test_median_of_singleton():
     assert median([4]) == 4
 
 
+def test_median_odd_number():
+    data = [1, 2, 3, 4, 5]
+    assert median(data) == 3
+
+
+def test_median_even_number():
+    data = [1, 2, 3, 4]
+    assert median(data) == 2.5
+
+
+def test_list_with_ordered_elements():
+    pass
+
+
+def test_list_with_reverse_ordered_elements():
+    pass
+
+
+def test_list_with_unordered_elements():
+    pass
+
+
 def test_median_rasis_value_error_on_empty_list():
+    """A test checking that requesting the median of an empty list raises a
+    ValueError exception"""
     with pytest.raises(ValueError):
         median([])
+
+
+def test_original_unchanged():
+    """A test that ensures that the median function leaves the original
+    data unchanged"""
+    pass
+
+
+def test_works_median_with_tuples():
+    """A test that ensures that the median function works for tuples as
+    well as lists"""
+    pass
