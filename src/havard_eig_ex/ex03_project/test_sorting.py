e36266e377b9a39990f70fd697892cf01188f013
Håvard <havardei@nmbu.no>
2019-10-02 12:22:58 +0200

    testing bubble

--- src/havard_eig_ex/ex03_project/test_sorting.py
+++ src/havard_eig_ex/ex03_project/test_sorting.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Håvard Brobakken Eig'
+__email__ = 'havardei@nmbu.no'
+
+
 def bubble_sort(dataa):
     data_list = list(dataa)
     for i in range(len(data_list) - 1, 0, -1):
@@ -16,3 +22,75 @@ if __name__ == "__main__":
                  (12, 8, 3, 1),
                  (8, 3, 12, 1)):
         print('{!s:>15} --> {!s:>15}'.format(data, bubble_sort(data)))
+
+
+def test_empty():
+        """Test that the sorting function works for empty list"""
+    assert bubble_sort([]) == []
+
+
+def test_single():
+    """Test that the sorting function works for single-element list"""
+    assert bubble_sort([1]) == [1]
+
+
+def test_sorted_is_not_original():
+    """
+    Test that the sorting function returns a new object.
+
+    Consider
+
+    data = [3, 2, 1]
+    sorted_data = bubble_sort(data)
+
+    Now sorted_data shall be a different object than data,
+    not just another name for the same object.
+    """
+    assert bubble_sort([3, 2, 1]) != [3, 2, 1]
+
+
+def test_original_unchanged():
+    """
+    Test that sorting leaves the original data unchanged.
+
+    Consider
+
+    data = [3, 2, 1]
+    sorted_data = bubble_sort(data)
+
+    Now data shall still contain [3, 2, 1].
+    """
+    data = [3, 2, 1]
+    sorted_data = bubble_sort(data)
+    assert sorted_data != data
+
+
+def test_sort_sorted():
+    """Test that sorting works on sorted data."""
+    data = [3, 2, 1]
+    sorted = bubble_sort(data)
+    assert sort_sorted(data) == sorted
+
+
+def test_sort_reversed():
+    """Test that sorting works on reverse-sorted data."""
+    data = [3, 2, 1]
+    sorted_data = bubble_sort(data)
+    reversed_sorted_data = sorted_data.reverse()
+    assert reversed_sorted_data == sorted_data
+
+
+def test_sort_all_equal():
+    """Test that sorting handles data with identical elements."""
+    pass
+
+
+def test_sorting():
+    """
+    Test sorting for various test cases.
+
+    This test case should test sorting of a range of data sets and
+    ensure that they are sorted correctly. These could be lists of
+    numbers of different length or lists of strings.
+    """
+    pass
