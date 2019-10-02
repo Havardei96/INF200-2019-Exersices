60e9e6e30313ab075d2ba240dcd9b4b7097d889a
Håvard <havardei@nmbu.no>
2019-10-02 13:29:12 +0200

    median start

---
+++ src/havard_eig_ex/ex03_project/test_median.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Håvard Brobakken Eig'
+__email__ = 'havardei@nmbu.no'
+
+
+def median(data):
+    """
+    Returns median of data.
+
+    :param data: An iterable of containing numbers
+    :return: Median of data
+    """
+
+    sdata = sorted(data)
+    n = len(sdata)
+    return (sdata[n//2] if n % 2 == 1
+        else 0.5 * (sdata[n//2 - 1] + sdata[n//2]))
+
+
+def test_median_of_singleton():
+    """tests that median funcion works for lists with single items"""
+    assert median([4]) == 4
+
+
+def test_median_rasis_value_error_on_empty_list():
+    with pytest.raises(ValueError):
+        median([])
