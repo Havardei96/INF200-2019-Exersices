ee657abb88c47015c29ef4d698edb80857cdae53
Håvard <havardei@nmbu.no>
2019-10-23 15:53:03 +0200

    print test

--- src/havard_eig_ex/ex04/walker.py
+++ src/havard_eig_ex/ex04/walker.py
@@ -35,3 +35,20 @@ class Walker:
         while not self.is_at_home():
             self.move()
         return self.moves
+
+
+if __name__ == "__main__":
+    print(f'Distance:   1 -> Path lengths: '
+          f'{[Walker(0, 1).walking_start_home() for _ in range(5)]}')
+    print(f'Distance:   2 -> Path lengths: '
+          f'{[Walker(0, 2).walking_start_home() for _ in range(5)]}')
+    print(f'Distance:   5 -> Path lengths: '
+          f'{[Walker(0, 5).walking_start_home() for _ in range(5)]}')
+    print(f'Distance:   10 -> Path lengths: '
+          f'{[Walker(0, 10).walking_start_home() for _ in range(5)]}')
+    print(f'Distance:   20 -> Path lengths: '
+          f'{[Walker(0, 20).walking_start_home() for _ in range(5)]}')
+    print(f'Distance:   50 -> Path lengths: '
+          f'{[Walker(0, 50).walking_start_home() for _ in range(5)]}')
+    print(f'Distance:   100 -> Path lengths: '
+          f'{[Walker(0, 100).walking_start_home() for _ in range(5)]}')
