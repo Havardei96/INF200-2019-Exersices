849b4201e5948b751398f4b96274ca6dd42f1fbb
Håvard <havardei@nmbu.no>
2019-10-23 13:41:51 +0200

    bug

--- src/havard_eig_ex/ex04/walker.py
+++ src/havard_eig_ex/ex04/walker.py
@@ -1,2 +1,37 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Håvard Brobakken Eig'
+__email__ = 'havardei@nmbu.no'
+
+import random
+
+
 class Walker:
-    pass
+    def __init__(self, start, home):
+        self.position = start
+        self.home = home
+        self.moves = 0
+
+    def move(self):
+        self.moves += 1
+        if random.randint(0, 1) == 1:
+            self.position += 1
+        else:
+            self.position -= 1
+
+    def is_at_home(self):
+        if self.position == self.home:
+            return True
+        else:
+            return False
+
+    def get_position(self):
+        return self.position
+
+    def get_steps(self):
+        return self.moves
+
+    def walking_start_home(self):
+        while not self.is_at_home():
+            self.move()
+        return self.moves
